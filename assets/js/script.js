fb34683deaa42aacb4a967325de5d2af336597a3
Alex Standfield <AlexStandfield@gmail.com>
2022-06-21 13:55:30 -0600

    ui enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,7 +142,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   let taskText = $("#modalTaskDescription").val();
   let taskDate = $("#modalDueDate").val();
@@ -180,16 +180,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this)
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this)
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target)
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target)
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     let tempArr = [];
@@ -223,14 +225,13 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function (event, ui) {
-    console.log("drop");
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -258,3 +259,11 @@ let auditTask = function(taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+
+// Automatic
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, (1000 * 60) * 30);
